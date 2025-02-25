85e539c37b7243933b821618ee6da516714fb2a2
Atikulla2001 <atikullahasib2001@gmail.com>
2025-02-25 11:21:38 +0600

    hsdn file is update

---
+++ style.js
@@ -0,0 +1,35 @@
+let button = document.querySelector("button")
+let css = document.querySelector(".css")
+let div = document.querySelector("div");
+
+
+
+button.addEventListener(("click"),function () {
+    css.href = `${location.protocol}//${location.host}/css/light_dark.css`
+    
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------- hide / show ------- //
+
+ //buttonn.addEventListener(("click"),function () {
+    //   div.classList = "box"
+    //   div.setAttribute("class" , "box")       
+    //   div.classList.toggle("box")            // on click show / hide 
+    //   div.classList.remove("box")           // remove
+    //   div.classList.add("box2")            // add
+ //})  
+
+
+// ----------- hide / show ------- //
